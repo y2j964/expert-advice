ea97b9f0e13dfebbcf5d257e7499fb6db3a5d13c
Justin Mooney <y2j964@aol.com>
2020-11-29 00:38:47 -0500

    Update navbar to match illustrations

--- app/components/nav-bar.js
+++ app/components/nav-bar.js
@@ -1,12 +1,12 @@
 import Component from "@glimmer/component";
 import { inject as service } from "@ember/service";
 import { action } from "@ember/object";
-import { tracked } from '@glimmer/tracking';
+import { tracked } from "@glimmer/tracking";
 
 export default class NavBarComponent extends Component {
   @service session;
 
-  @tracked collapsed = false;
+  @tracked collapsed = true;
 
   @action
   async logout() {
